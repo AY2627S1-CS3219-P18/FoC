85ac0c75ef4bbb1f81418c8aba8867c6a6b5f9db
wen xi <tngwenxi@gmail.com>
2026-09-26 01:57:16 +0800

    Remove console.log statements

--- order-service/src/controllers/requests.controller.ts
+++ order-service/src/controllers/requests.controller.ts
@@ -5,16 +5,11 @@ import * as requestsService from '../services/requests.service.js';
 import { ErrorMessage } from '../constants/errors.js';
 
 export const createRequest = (req: Request, res: Response) => {
-  console.log(req.body);
   const details: createRequestPayload = req.body;
   const errors = requestsService.createRequest(details);
   if (!!errors.length) {
-    console.log('The payload is invalid for the following reasons:\n');
-
     const errorMessages = errors.map((error) => ErrorMessage[error]);
     const errorMessageString = errorMessages.join('\n');
-
-    console.log(errorMessageString);
     res.status(400).json({ message: errorMessageString });
   }
   res.status(200).json({ message: 'Successfully created request' });
